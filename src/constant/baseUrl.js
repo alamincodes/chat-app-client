1d6e2a688d72bfebefa1cf34a271c940ef84f8b3
Md Alamin <mdalamincodes@gmail.com>
2024-11-24 13:47:38 +0600

    change url

--- src/constant/baseUrl.js
+++ src/constant/baseUrl.js
@@ -1,2 +1,3 @@
-export const BASE_URL = "https://chat-app-server-7hgs.onrender.com";
+// export const BASE_URL = "https://chat-app-server-7hgs.onrender.com";
+export const BASE_URL = "https://chatapp.khiraikandi.com";
 // export const BASE_URL = "http://localhost:5001";
